382efa84cfd0ce6b01100ff0dbde356cc15acc56
Inmaculada Moyano <137101898+inmamoyano@users.noreply.github.com>
2023-09-07 20:55:40 +0200

    Update ZZtest.cc

--- ZZtest.cc
+++ ZZtest.cc
@@ -54,7 +54,7 @@ namespace Rivet {
 
       // book histograms
       book(_hist_ZZ, "d01-x01-y01");
-      book(_h_Z_mass ,"Z_mass", 40, 0.0, 200.0);
+      
     }
 
 
@@ -106,7 +106,7 @@ namespace Rivet {
 
       // fill the histograms
       _hist_ZZ->fill(5020);
-      _h_Z_mass->fill((dressedLeptons.at(0).momentum() + dressedLeptons.at(1).momentum()).mass()) ;      
+          
     }
 
     /// @brief Normalise histograms after the run
@@ -117,7 +117,7 @@ namespace Rivet {
   private:
     // Declaration of histograms
     Histo1DPtr _hist_ZZ;
-    Histo1DPtr _h_Z_mass;
+    
   };
 
   RIVET_DECLARE_PLUGIN(ZZtest);
